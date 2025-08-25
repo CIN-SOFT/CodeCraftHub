5ebed194d7780e4c7b236071eb179c41dc3c3a70
Aliou Thiema <aliouthiema@gmail.com>
2025-08-25 18:59:32 -0400

    add docker configuration

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,20 @@
+// src/routes/userRoutes.js
 const express = require('express');
+// Importing the controller functions for user registration and login
 const { registerUser, loginUser } = require('../controllers/userController');
+
+// Creating a router object to define user-related routes
 const router = express.Router();
 
+// Route for user registration
+// This route handles POST requests to '/api/users/register'
+// It calls the registerUser function from the userController
 router.post('/register', registerUser);
+
+// Route for user login
+// This route handles POST requests to '/api/users/login'
+// It calls the loginUser function from the userController
 router.post('/login', loginUser);
 
+// Exporting the router to be used in the main application
 module.exports = router;
